8b9c68268134c4af018501da9fbbb9796a067566
dulceonejs <dulceonejs@gmail.com>
2020-12-25 02:22:55 +0200

    Backend | fix linter errors

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -3,16 +3,16 @@ const jwt = require('jsonwebtoken');
 const secret = process.env.SECRET;
 
 module.exports = (req, res, next) => {
-    const token = req.headers['x-access-token'];
-    if (!token) {
-        return res.status(401).json({ message: 'Not access token' });
-    }
+  const token = req.headers['x-access-token'];
+  if (!token) {
+    return res.status(401).json({ message: 'Not access token' });
+  }
 
-    try {
-        const user = jwt.verify(token, secret);
-        req.user = user;
-        next();
-    } catch {
-        res.status(401).json({ message: 'Failed to authenticate token.' });
-    }
+  try {
+    const user = jwt.verify(token, secret);
+    req.user = user;
+    next();
+  } catch {
+    res.status(401).json({ message: 'Failed to authenticate token.' });
+  }
 }
